8cb071384dfb1bded3e9029ba3ca425239da9498
Paweł Łazicki <pawelllaz@gmail.com>
2019-12-17 11:10:04 +0100

    formatted

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,21 +1,16 @@
 package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
+    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
 
-	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-	
-	@Override
-	public int countOut() {
-		while (!callCheck())
-			
-		temp.countIn(super.countOut());
-		
-		int ret = temp.countOut();
-		
-		while (!temp.callCheck())
-			
-		countIn(temp.countOut());
-		
-		return ret;
-	}
+    @Override
+    public int countOut() {
+        while (!callCheck())
+            temp.countIn(super.countOut());
+        int ret = temp.countOut();
+        while (!temp.callCheck())
+            countIn(temp.countOut());
+
+        return ret;
+    }
 }
